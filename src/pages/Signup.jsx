34d0a262342558e293677a95c3150ed9944056e6
Dilshan-Kavishaka-Karunarathna <dilshankavishka667@gmail.com>
2025-02-25 21:07:42 +0530

    create sign up page
    create sign up page using react-icons, react-router, shadcn ui components. when  user enter email and password and click the sign up button email and passwords are show the console.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,38 +1,92 @@
 import React from 'react'
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from 'react-router';
+import { useState } from 'react';
 
 
 
 const Signup = () => {
+    const[Email,setEmail] = useState("");
+    const[password,setPassword]= useState("");
+
+    const handleSignup = (e)=>{
+        e.preventDefault();
+        console.log("sign up with",Email,password);
+    }
   return (
     <div className='bg-gray-50 justify-center items-center min-h-screen'>
         <div className='max-w-md w-full mx-auto p-6'>
-            <div className='bg-white rounded-lg shadow-lg space-y-6 p-6'>
+            <div className='bg-white rounded-lg shadow-lg p-8 space-y-6'>
                 <div className='text-center space-y-2'>
-                    <h2 className='text-2xl font-semibold'>
+                    <h2 className='text-2xl font-semibold tracking-tight'>
                         Create Your Account
                     </h2>
-                    <p>
+                    <p className='text-muted-foreground'>
                         You can create your own account with us!
                     </p>
                     </div>
-                    <form>
+                    <form className='space-y-6' onSubmit={handleSignup}>
+                        <div className='space-y-2'>
                         <label htmlFor="Email">Email</label>
                         <Input
                          type="email"
                          placeholder ="Enter Your Email"
-                        // value={email}
+                         value={Email}
                          onChange = {(e)=>setEmail(e.target.value)}
                          required
+                        />
+                        </div>
+                        <div className='space-y-2'>
+                            <lable htmlFor="Password">Password</lable>
+                            <Input
+                            type="password"
+                            placeholder="Enter Password"
+                            value={password}
+                            onChange={(e)=>setPassword(e.target.value)}
+                            required
+                            />
+                        </div>
+                        <div>
+                            <Button className='w-full' type='submit'>
+                                Sign Up
+                            </Button>
+                      
 
-                         />
+                        </div>
+                        </form>
+                        <div className='relative'>
+                            <div className='absolute inset-0 flex items-center'>
+                                <span className='w-full border-t'/>
+                            </div>
+                            <div className='relative flex justify-center text-xs uppercase'>
+                                <span className='bg-white px-2 text-muted-foreground'>
+                                OR CONTINUE WITH 
+                                </span>                       
+                            </div>
+                        </div>
+                        <Button
+                        variant="outline"
+                        className='w-full'
+                        >
+                            <FcGoogle />
+                            Continue With Google
+                        </Button>
+                        <div className='text-center text-sm'>
+                            All Ready Have an Account? {" "}
+                            <Link
+                                href="/Sign In"
+                                className="font-medium text-primary hover:underline"
+                            >
+                                    Sign in
+                                    </Link>
+                        </div>
 
-
-                    </form>
-
-                       
-                
+                        <div className='text-muted-foreground text-center text-sm'>
+                        Secured by Clerk
+                        </div>
 
             </div>
 
